96ec4700d9062b8c9d61cf77e1bab83124bcffdd
CGC <70499508+cgconverse@users.noreply.github.com>
2020-11-01 23:00:40 -0800

    submitted

---
+++ script.js
@@ -0,0 +1,14 @@
+var upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var lowerCase ="abcdefghijklmnopqrstuvwxyz";
+
+var Number =1234567890;
+
+var Char ="~`!#$%^&*+=-[]\\\';,/{}|\":<>?";
+
+var confirmStart = confirm("Would you like to create a password?");
+
+if (confirmStart) {
+        prompt("How long do you want your password to be?");
+      }
+
