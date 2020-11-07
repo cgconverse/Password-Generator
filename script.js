07e5a21519645358c21f3f0223574c920f030899
CGC <70499508+cgconverse@users.noreply.github.com>
2020-11-07 13:38:22 -0800

    updated js file to fix password generator function

--- script.js
+++ script.js
@@ -1,22 +1,68 @@
-var upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var generateBtn = document.querySelector("#generate");
 
-var lowerCase ="abcdefghijklmnopqrstuvwxyz";
+alert("Welcome to Password Generator! To begin, click the Generate Password button.")
 
-var Number =1234567890;
+// 'Generate Password' button
+generateBtn.addEventListener("click", createPassword);
 
-var Char ="~`!#$%^&*+=-[]\\\';,/{}|\":<>?";
+// This function displays the new password in the password text area.
+function createPassword() {
+    var password = generatePassword();
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var Char = "!')(*+,-.$/:;<=>%&?@[#\]^_`{|}~";
+    var num = "0123456789";
+    var passwordText = []
+    var result = []
+    upperCase = upperCase.split("")
+    lowerCase = lowerCase.split("")
+    Char = Char.split("")
+    num = num.split("")
+    passwordText.value = password;
 
-var confirmStart = confirm("Would you like to create a password?");
+    
+    //The following prompts and confirms determine which criteria to include in the user's password
+    var pwLength = prompt("Choose how long you want your password to be between 8-128 characters.")
 
-if (confirmStart) {
-        prompt("How long do you want your password to be?");
-      }
+    
+    if (pwLength < 8 && pwLength > 128) {
+        alert("Password should be between 8 and 128 characters")
+    }
+    if (confirm("Would you like to include upper case letters?")) {
+        for (var i = 0; i < upperCase.length; i++) {
+            result.push(upperCase[i])
+        }
+        if (confirm("Would you like to include lower case letters?")) {
+            for (var i = 0; i < lowerCase.length; i++) {
+                result.push(lowerCase[i])
+            }
+        }
+    }
+    
+    if (confirm("Would you like to include numbers?")) {
+        for (var i = 0; i < num.length; i++) {
+            result.push(num[i])
+        }
+    }
+    
+    if (confirm("Would you like to include special characters?")) {
+        for (var i = 0; i < Char.length; i++) {
+            result.push(Char[i])
+        }
 
-var confirmLowerCase =confirm("Do you want to include lower case letters?")
+    }
 
-var confirmUpperCase =confirm("Do you want to include upper case letters?")
+    generatePassword(result, pwLength)
+}
 
-var confirmNumbers =confirm("Do you want to include numbers?")
+//This function generates the password according to the user's selected criteria.
+function generatePassword(characterCatcher, pwLength) {
 
-var confirmChar =confirm("Do you want to include special characters?")
-
+    var password = ""
+    for (var i = 0; i < pwLength; i++) {
+        password += characterCatcher[Math.floor(Math.random() * characterCatcher.length)]
+    }
+    
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
